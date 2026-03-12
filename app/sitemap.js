caeb29d1579663fc897a5eccaeb44bf9ab459a70
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2026-03-12 15:52:00 +0100

    Added job board one

--- app/sitemap.js
+++ app/sitemap.js
@@ -3,14 +3,20 @@ export default function sitemap() {
     {
       url: "https://www.scippra.com",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 1.0,
     },
     {
       url: "https://www.scippra.com/about",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: "https://www.scippra.com/contact",
       lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.6,
     },
   ];
 }
