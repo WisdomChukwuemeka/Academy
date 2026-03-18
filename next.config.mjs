b68e938352e9a9fcaf574d85085734ce4c95eaf1
wisdom chukwuemeka <wisdomchukwuemeka97@gmail.com>
2026-03-18 10:17:12 +0100

    updated hero page

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    formats:['image/avif', 'image/webp'],
-    minimumCacheTTL: 31536000, // Forces aggressive caching (1 year)
+    formats: ["image/avif", "image/webp"],
+    minimumCacheTTL: 31536000, // 1 year aggressive caching
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/api/clerk/:path*",
+        destination: "https://clerk.scippra.com/:path*",
+      },
+    ];
   },
 };
+
 export default nextConfig;
